083bd2ac59b0636c5a95118ede06df73d90a19dd
Kevin S <alphalop42@gmail.com>
2020-11-21 17:59:17 -0800

    Incremental General Component -added base functionality for handleSubmit

--- src/components/General.js
+++ src/components/General.js
@@ -8,6 +8,7 @@ class General extends React.Component {
             lName: "",
             email: "",
             phone: "000-000-0000",
+            isSubmitted: false,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,26 +26,37 @@ class General extends React.Component {
         console.log("lname:" + this.state.lName);
         console.log("email:" + this.state.email);
         console.log("num:" + this.state.phone);
+        this.setState({isSubmitted: true});
         e.preventDefault();
     };
 
     render(){
-        const {fName, lName, email, phone} = this.state;
-        return (
-            <div className="card general">
-                <form className="genForm" onSubmit={this.handleSubmit}>
-                    <label for="genFirstName" className="label">First Name:</label>
-                    <input id="genFirstName" name ="fName" type="text" value={fName} onChange={this.handleChange}/>
-                    <label for="genLastName" className="label">Last Name:</label>
-                    <input id="genLastName" name ="lName" type="text" value={lName} onChange={this.handleChange}/>
-                    <label for="genEmail">Email:</label>
-                    <input id="genEmail"  name ="email" type="text" value={email} onChange={this.handleChange}/>
-                    <label for="genPhnoe">Phone Number:</label>
-                    <input id="genPhone"  name ="phone" type="text" value={phone} onChange={this.handleChange}/>
-                    <input type="submit" task="Submit"></input>
-                </form>
-            </div>
-        );
+        const {fName, lName, email, phone, isSubmitted} = this.state;
+        if(isSubmitted){
+            return(
+                <div className="card general">
+                    Submitted worked!
+                </div>
+            )
+        }
+
+        if(!isSubmitted){
+            return (
+                <div className="card general">
+                    <form className="genForm" onSubmit={this.handleSubmit}>
+                        <label for="genFirstName" className="label">First Name:</label>
+                        <input id="genFirstName" name ="fName" type="text" value={fName} onChange={this.handleChange}/>
+                        <label for="genLastName" className="label">Last Name:</label>
+                        <input id="genLastName" name ="lName" type="text" value={lName} onChange={this.handleChange}/>
+                        <label for="genEmail">Email:</label>
+                        <input id="genEmail"  name ="email" type="text" value={email} onChange={this.handleChange}/>
+                        <label for="genPhnoe">Phone Number:</label>
+                        <input id="genPhone"  name ="phone" type="text" value={phone} onChange={this.handleChange}/>
+                        <input type="submit" task="Submit"></input>
+                    </form>
+                </div>
+            );
+        }
     }   
 };
 
